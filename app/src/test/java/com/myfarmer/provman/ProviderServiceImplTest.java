3e735111b052a0d345ea809dff6dcbdbe0499e23
melinaschweizer <mschweiz@amazon.com>
2021-09-22 12:00:58 +0200

    spacing cleanup

--- app/src/test/java/com/myfarmer/provman/ProviderServiceImplTest.java
+++ app/src/test/java/com/myfarmer/provman/ProviderServiceImplTest.java
@@ -19,114 +19,113 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
 @ExtendWith(MockitoExtension.class)
 class ProviderServiceImplTest {
 
 	// allow this class to use Mocked objects
 	@InjectMocks
-    private ProviderServiceImpl service;
+	private ProviderServiceImpl service;
 
-    // Mock the DB component
-    @Mock
+	// Mock the DB component
+	@Mock
 	private ProviderDao dao;
 
 	@Test
 	void testFindById() {
-        Provider provider = new Provider(1,"name",LocalDate.now(),"nationality","code");
-        Mockito.doReturn(provider).when(dao).findById(1);
+		Provider provider = new Provider(1, "name", LocalDate.now(), "nationality", "code");
+		Mockito.doReturn(provider).when(dao).findById(1);
 
-        // Execute the service call
-        Provider returnedProvider = service.findById(1);
+		// Execute the service call
+		Provider returnedProvider = service.findById(1);
 
-        // Assert the response
-        assertNotNull(returnedProvider, "Provider was not found");
-        assertSame(returnedProvider, provider, "The Provider returned was not the same as the mock");
+		// Assert the response
+		assertNotNull(returnedProvider, "Provider was not found");
+		assertSame(returnedProvider, provider, "The Provider returned was not the same as the mock");
 	}
 
 	@Test
 	void testFindAllProviders() {
 		List<Provider> allProviders = new ArrayList<Provider>();
-		Provider provider = new Provider(1,"name",LocalDate.now(),"nationality","code");
-		Provider provider2 = new Provider(2,"name2",LocalDate.now(),"nationality2","code2");
+		Provider provider = new Provider(1, "name", LocalDate.now(), "nationality", "code");
+		Provider provider2 = new Provider(2, "name2", LocalDate.now(), "nationality2", "code2");
 		allProviders.add(provider);
 		allProviders.add(provider2);
-        Mockito.doReturn(allProviders).when(dao).findAllProviders();
+		Mockito.doReturn(allProviders).when(dao).findAllProviders();
 
-        // Execute the service call
-        List<Provider> returnedProviders = service.findAllProviders();
+		// Execute the service call
+		List<Provider> returnedProviders = service.findAllProviders();
 
-        // Assert the response
-        assertTrue(returnedProviders != null && !returnedProviders.isEmpty(), "Providers not found");
-        assertTrue(returnedProviders.size() == 2, "The Provider returned was not the same as the mock");
+		// Assert the response
+		assertTrue(returnedProviders != null && !returnedProviders.isEmpty(), "Providers not found");
+		assertTrue(returnedProviders.size() == 2, "The Provider returned was not the same as the mock");
 	}
-	
+
 	@Test
 	void testFindByIdNotExists() {
-        // Execute the service call
-        Provider returnedProvider = service.findById(999);
+		// Execute the service call
+		Provider returnedProvider = service.findById(999);
 
-        // Assert the response
-        assertTrue(returnedProvider == null, "Bad Provider returned");
+		// Assert the response
+		assertTrue(returnedProvider == null, "Bad Provider returned");
 	}
-	
+
 	@Test
 	void testUpdateProvider() {
 		LocalDate now = LocalDate.now();
-		Provider provider = new Provider(1,"name-a", now, "nationality-a", "code-a");
-        Mockito.doReturn(provider).when(dao).findById(1);
+		Provider provider = new Provider(1, "name-a", now, "nationality-a", "code-a");
+		Mockito.doReturn(provider).when(dao).findById(1);
 
-        Provider providerUpdated = new Provider(1, "name-b", now, "nationality-b", "code-b");
-        // Execute the service call
-        service.updateProvider(providerUpdated);
-        
-        // check that the original provider now has vars matching the updated provider
-        assertTrue(provider.equals(providerUpdated), "The Provider returned was not the same as the mock");
-                
+		Provider providerUpdated = new Provider(1, "name-b", now, "nationality-b", "code-b");
+		// Execute the service call
+		service.updateProvider(providerUpdated);
+
+		// check that the original provider now has vars matching the updated provider
+		assertTrue(provider.equals(providerUpdated), "The Provider returned was not the same as the mock");
+
 	}
-	
+
 	@Test
 	void testFindProviderByCode() {
-        Provider provider = new Provider(1,"name",LocalDate.now(),"nationality","code");
-        Mockito.doReturn(provider).when(dao).findProviderByCode(Mockito.any());
+		Provider provider = new Provider(1, "name", LocalDate.now(), "nationality", "code");
+		Mockito.doReturn(provider).when(dao).findProviderByCode(Mockito.any());
 
-        // Execute the service call
-        Provider returnedProvider = service.findProviderByCode("a");
+		// Execute the service call
+		Provider returnedProvider = service.findProviderByCode("a");
 
-        // Assert the response
-        assertNotNull(returnedProvider, "Provider was not found");
-        assertSame(returnedProvider, provider, "The Provider returned was not the same as the mock");
+		// Assert the response
+		assertNotNull(returnedProvider, "Provider was not found");
+		assertSame(returnedProvider, provider, "The Provider returned was not the same as the mock");
 	}
-	
+
 	@Test
 	void testFindProviderByCodeNotExists() {
-        // Execute the service call
-        Provider returnedProvider = service.findProviderByCode("a");
+		// Execute the service call
+		Provider returnedProvider = service.findProviderByCode("a");
 
-        // Assert the response
-        assertNull(returnedProvider, "Bad Provider was found");
+		// Assert the response
+		assertNull(returnedProvider, "Bad Provider was found");
 	}
-	
+
 	@Test
 	void testIsProviderCodeUniqueSameExists() {
-		Provider provider = new Provider(1,"name-a", LocalDate.now(), "nationality-a", "code-a");
+		Provider provider = new Provider(1, "name-a", LocalDate.now(), "nationality-a", "code-a");
 		Mockito.when(dao.findProviderByCode(Mockito.any())).thenReturn(provider);
-	
+
 		boolean retVal = service.isProviderCodeUnique(1, "a");
 		assertTrue(retVal);
 	}
-	
+
 	@Test
 	void testIsProviderCodeUniqueDoesntExist() {
 		boolean retVal = service.isProviderCodeUnique(1, "a");
 		assertTrue(retVal);
 	}
-	
+
 	@Test
 	void testIsProviderCodeUniqueOtherExists() {
-		Provider provider = new Provider(1,"name-a", LocalDate.now(), "nationality-a", "code-a");
+		Provider provider = new Provider(1, "name-a", LocalDate.now(), "nationality-a", "code-a");
 		Mockito.when(dao.findProviderByCode(Mockito.any())).thenReturn(provider);
-	
+
 		boolean retVal = service.isProviderCodeUnique(2, "a");
 		assertFalse(retVal);
 	}
--- app/src/test/java/com/myfarmer/provman/controller/ProvmanControllerTest.java
+++ app/src/test/java/com/myfarmer/provman/controller/ProvmanControllerTest.java
@@ -27,25 +27,24 @@ import org.springframework.context.MessageSource;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 
-
 @ExtendWith(MockitoExtension.class)
 class ProvmanControllerTest {
 
 	@InjectMocks
-	ProvmanController controller;    
-	
-    @Mock
+	ProvmanController controller;
+
+	@Mock
 	ProviderService providerService;
-    
-    @Mock
+
+	@Mock
 	ProductService productService;
-    
-    @Mock
-	ProductPricingService pricingService;	
-    
-    @Mock
+
+	@Mock
+	ProductPricingService pricingService;
+
+	@Mock
 	MessageSource messageSource;
-    
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
@@ -56,220 +55,213 @@ class ProvmanControllerTest {
 
 	@Test
 	void testListProviders() {
-        Provider provider = new Provider(1,"name",LocalDate.now(),"nationality","code");
-        Mockito.doReturn(new ArrayList<Provider>(List.of(provider))).when(providerService).findAllProviders();        
-        ModelMap modelMap = new ModelMap();
-        String retVal = controller.listProviders(modelMap);
-        
-        assertTrue(modelMap.containsKey("providers"));
-        @SuppressWarnings("unchecked")
+		Provider provider = new Provider(1, "name", LocalDate.now(), "nationality", "code");
+		Mockito.doReturn(new ArrayList<Provider>(List.of(provider))).when(providerService).findAllProviders();
+		ModelMap modelMap = new ModelMap();
+		String retVal = controller.listProviders(modelMap);
+
+		assertTrue(modelMap.containsKey("providers"));
+		@SuppressWarnings("unchecked")
 		List<Provider> returnedList = (List<Provider>) modelMap.get("providers");
-        assertTrue(returnedList.size() == 1);
-        assertTrue(((Provider) returnedList.get(0)).equals(provider));
-        assertSame(Views.ALLPROVIDERS.getViewName(), retVal);    	
-	}	
-	
+		assertTrue(returnedList.size() == 1);
+		assertTrue(((Provider) returnedList.get(0)).equals(provider));
+		assertSame(Views.ALLPROVIDERS.getViewName(), retVal);
+	}
+
 	@Test
 	void testNewProvider() {
-        ModelMap modelMap = new ModelMap();
-        String retVal = controller.newProvider(modelMap);
+		ModelMap modelMap = new ModelMap();
+		String retVal = controller.newProvider(modelMap);
 
-        assertTrue(modelMap.containsKey("provider"));        
-		assertNotNull(modelMap.get("provider"));	
-		
-		assertTrue(modelMap.containsKey("edit"));     
+		assertTrue(modelMap.containsKey("provider"));
+		assertNotNull(modelMap.get("provider"));
+
+		assertTrue(modelMap.containsKey("edit"));
 		assertFalse((Boolean) modelMap.get("edit"));
-		
-		assertSame(Views.REGISTRATION.getViewName(), retVal);    	
+
+		assertSame(Views.REGISTRATION.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testSaveProvider() {
 		Provider provider = new Provider();
-		ModelMap modelMap = new ModelMap();		
+		ModelMap modelMap = new ModelMap();
 		BindingResult result = Mockito.mock(BindingResult.class);
-	    Mockito.when(result.hasErrors()).thenReturn(false);
-	    Mockito.when(providerService.isProviderCodeUnique(Mockito.any(), Mockito.any()))
-				.thenReturn(true);       
-	    
-	    String retVal = controller.saveProvider(provider, result, modelMap);
-	    assertTrue(modelMap.containsKey("success"));     
-	    assertSame(Views.SUCCESS.getViewName(), retVal);    	
+		Mockito.when(result.hasErrors()).thenReturn(false);
+		Mockito.when(providerService.isProviderCodeUnique(Mockito.any(), Mockito.any())).thenReturn(true);
+
+		String retVal = controller.saveProvider(provider, result, modelMap);
+		assertTrue(modelMap.containsKey("success"));
+		assertSame(Views.SUCCESS.getViewName(), retVal);
 	}
-	
+
 	@Test
-	void testSaveProviderBindingErrors() {		
+	void testSaveProviderBindingErrors() {
 		BindingResult result = Mockito.mock(BindingResult.class);
-	    Mockito.when(result.hasErrors()).thenReturn(true);
-	    String retVal = controller.saveProvider(new Provider(), result, new ModelMap());
-	    assertSame(Views.REGISTRATION.getViewName(), retVal);    	
+		Mockito.when(result.hasErrors()).thenReturn(true);
+		String retVal = controller.saveProvider(new Provider(), result, new ModelMap());
+		assertSame(Views.REGISTRATION.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testSaveProviderNonUniqueProvider() {
 		ModelMap modelMap = new ModelMap();
-		Provider provider = new Provider(1,"name",LocalDate.now(),"nationality","code");
+		Provider provider = new Provider(1, "name", LocalDate.now(), "nationality", "code");
 		BindingResult result = Mockito.mock(BindingResult.class);
-	    Mockito.when(result.hasErrors()).thenReturn(false);
-	    Mockito.when(providerService.isProviderCodeUnique(Mockito.any(), Mockito.any()))
-				.thenReturn(false);       
-	    
-	    String retVal = controller.saveProvider(provider, result, modelMap);
-	    assertSame(Views.REGISTRATION.getViewName(), retVal);    	
+		Mockito.when(result.hasErrors()).thenReturn(false);
+		Mockito.when(providerService.isProviderCodeUnique(Mockito.any(), Mockito.any())).thenReturn(false);
+
+		String retVal = controller.saveProvider(provider, result, modelMap);
+		assertSame(Views.REGISTRATION.getViewName(), retVal);
 	}
-	
+
 	@Test
 	public void testEditProvider() {
 		ModelMap modelMap = new ModelMap();
 		Provider provider = new Provider();
-		Mockito.when(providerService.findProviderByCode(Mockito.any()))
-				.thenReturn(provider);    
-		
+		Mockito.when(providerService.findProviderByCode(Mockito.any())).thenReturn(provider);
+
 		String retVal = controller.editProvider("a", modelMap);
-		assertTrue(modelMap.containsKey("provider")); 
-		assertSame(provider, modelMap.get("provider"));	
-		
-		assertTrue(modelMap.containsKey("edit"));  
-		assertTrue((Boolean) modelMap.get("edit"));  
-		
-		assertSame(Views.REGISTRATION.getViewName(), retVal);   
+		assertTrue(modelMap.containsKey("provider"));
+		assertSame(provider, modelMap.get("provider"));
+
+		assertTrue(modelMap.containsKey("edit"));
+		assertTrue((Boolean) modelMap.get("edit"));
+
+		assertSame(Views.REGISTRATION.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testUpdateProvider() {
-		Provider provider = new Provider(1,"name",LocalDate.now(),"nationality","code");
-		ModelMap modelMap = new ModelMap();	
-		
+		Provider provider = new Provider(1, "name", LocalDate.now(), "nationality", "code");
+		ModelMap modelMap = new ModelMap();
+
 		BindingResult result = Mockito.mock(BindingResult.class);
-	    Mockito.when(result.hasErrors()).thenReturn(false);
-	    Mockito.when(providerService.isProviderCodeUnique(Mockito.any(), Mockito.any()))
-				.thenReturn(true);       
-	    
-	    String retVal = controller.updateProvider(provider, result, modelMap, "a");
-	    assertTrue(modelMap.containsKey("success"));     
-	    assertSame(Views.SUCCESS.getViewName(), retVal);    	
+		Mockito.when(result.hasErrors()).thenReturn(false);
+		Mockito.when(providerService.isProviderCodeUnique(Mockito.any(), Mockito.any())).thenReturn(true);
+
+		String retVal = controller.updateProvider(provider, result, modelMap, "a");
+		assertTrue(modelMap.containsKey("success"));
+		assertSame(Views.SUCCESS.getViewName(), retVal);
 	}
-	
+
 	@Test
-	void testUpdateProviderBindingErrors() {		
+	void testUpdateProviderBindingErrors() {
 		BindingResult result = Mockito.mock(BindingResult.class);
-	    Mockito.when(result.hasErrors()).thenReturn(true);
-	    String retVal = controller.updateProvider(new Provider(), result, new ModelMap(), "a");
-	    assertSame(Views.REGISTRATION.getViewName(), retVal);    	
+		Mockito.when(result.hasErrors()).thenReturn(true);
+		String retVal = controller.updateProvider(new Provider(), result, new ModelMap(), "a");
+		assertSame(Views.REGISTRATION.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testUpdateProviderNonUniqueProvider() {
-		ModelMap modelMap = new ModelMap();		
+		ModelMap modelMap = new ModelMap();
 		BindingResult result = Mockito.mock(BindingResult.class);
-	    Mockito.when(result.hasErrors()).thenReturn(false);
-	    Mockito.when(providerService.isProviderCodeUnique(Mockito.any(), Mockito.any()))
-				.thenReturn(false);       
-	    
-	    String retVal = controller.updateProvider(new Provider(), result, modelMap, "a");
-	    assertSame(Views.REGISTRATION.getViewName(), retVal);    	
+		Mockito.when(result.hasErrors()).thenReturn(false);
+		Mockito.when(providerService.isProviderCodeUnique(Mockito.any(), Mockito.any())).thenReturn(false);
+
+		String retVal = controller.updateProvider(new Provider(), result, modelMap, "a");
+		assertSame(Views.REGISTRATION.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testDeleteProvider() {
 		ModelMap modelMap = new ModelMap();
-		Provider provider = new Provider(1,"name",LocalDate.now(),"nationality","code");
-        Mockito.doReturn(new ArrayList<Provider>(List.of(provider))).when(providerService).findAllProviders();  
-        
+		Provider provider = new Provider(1, "name", LocalDate.now(), "nationality", "code");
+		Mockito.doReturn(new ArrayList<Provider>(List.of(provider))).when(providerService).findAllProviders();
+
 		String retVal = controller.deleteProvider("a", modelMap);
 		assertTrue(modelMap.containsKey("providers"));
-        @SuppressWarnings("unchecked")
+		@SuppressWarnings("unchecked")
 		List<Provider> returnedList = (List<Provider>) modelMap.get("providers");
-        assertTrue(returnedList.size() == 1);
-        assertTrue(((Provider) returnedList.get(0)).equals(provider));
-        assertSame(Views.ALLPROVIDERS.getViewName(), retVal);    	
+		assertTrue(returnedList.size() == 1);
+		assertTrue(((Provider) returnedList.get(0)).equals(provider));
+		assertSame(Views.ALLPROVIDERS.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testNewProduct() {
 		ModelMap modelMap = new ModelMap();
 
 		String retVal = controller.newProduct(1, modelMap);
-		assertTrue(modelMap.containsKey("product"));		
+		assertTrue(modelMap.containsKey("product"));
 		Product product = (Product) modelMap.get("product");
 		assertSame(product.getFarmId(), 1);
 
-		assertTrue(modelMap.containsKey("edit"));  
-		assertFalse((Boolean) modelMap.get("edit"));  
-		assertSame(Views.PRODUCT.getViewName(), retVal);    	
+		assertTrue(modelMap.containsKey("edit"));
+		assertFalse((Boolean) modelMap.get("edit"));
+		assertSame(Views.PRODUCT.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testSaveProduct() {
 		ModelMap modelMap = new ModelMap();
 		Product product = helperCreateProduct();
 
 		String retVal = controller.saveProduct(product, modelMap);
-		assertTrue(modelMap.containsKey("returnPage"));  
-		assertSame(Views.PRODUCT.getViewName(), (String) modelMap.get("returnPage"));  
-		
-		assertTrue(modelMap.containsKey("farmId"));  
-		assertSame(product.getFarmId(), (Integer) modelMap.get("farmId"));  
-		
-		assertTrue(modelMap.containsKey("success"));  
-		assertSame(Views.SUCCESS.getViewName(), retVal);  
+		assertTrue(modelMap.containsKey("returnPage"));
+		assertSame(Views.PRODUCT.getViewName(), (String) modelMap.get("returnPage"));
+
+		assertTrue(modelMap.containsKey("farmId"));
+		assertSame(product.getFarmId(), (Integer) modelMap.get("farmId"));
+
+		assertTrue(modelMap.containsKey("success"));
+		assertSame(Views.SUCCESS.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testGetProducts() {
 		ModelMap modelMap = new ModelMap();
 		Product product = helperCreateProduct();
 		Mockito.when(productService.getProductsByFarmId(Mockito.any()))
 				.thenReturn(new ArrayList<Product>(List.of(product)));
-		
-	
+
 		String retVal = controller.getProducts(1, modelMap);
 		assertTrue(modelMap.containsKey("products"));
-        @SuppressWarnings("unchecked")
+		@SuppressWarnings("unchecked")
 		List<Product> returnedList = (List<Product>) modelMap.get("products");
-        assertTrue(returnedList.size() == 1);
-        assertTrue(((Product) returnedList.get(0)).equals(product));
-        
-        assertTrue(modelMap.containsKey("farmId"));  
-		assertSame(product.getFarmId(), 1);  
-        assertSame(Views.PRODUCTLIST.getViewName(), retVal);    
+		assertTrue(returnedList.size() == 1);
+		assertTrue(((Product) returnedList.get(0)).equals(product));
+
+		assertTrue(modelMap.containsKey("farmId"));
+		assertSame(product.getFarmId(), 1);
+		assertSame(Views.PRODUCTLIST.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testEditProduct() {
 		ModelMap modelMap = new ModelMap();
 		Product product = helperCreateProduct();
-		Mockito.when(productService.findById(Mockito.any()))
-				.thenReturn(product);
+		Mockito.when(productService.findById(Mockito.any())).thenReturn(product);
 
 		String retVal = controller.editProduct(product.getId(), modelMap);
-		assertTrue(modelMap.containsKey("product"));		
+		assertTrue(modelMap.containsKey("product"));
 		Product retProduct = (Product) modelMap.get("product");
 		assertSame(retProduct, product);
-		
+
 		assertTrue(modelMap.containsKey("edit"));
 		assertTrue((Boolean) modelMap.get("edit"));
 		assertSame(Views.PRICINGLIST.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testUpdateProduct() {
 		ModelMap modelMap = new ModelMap();
 		ProductAndPrice productAndPrice = new ProductAndPrice();
 		productAndPrice.setProduct(new Product());
 		productAndPrice.getProduct().setFarmId(1);
-			
+
 		String retVal = controller.updateProduct(productAndPrice, modelMap);
 		assertTrue(modelMap.containsKey("returnPage"));
 		assertSame(Views.PRODUCT.getViewName(), (String) modelMap.get("returnPage"));
-		
+
 		assertTrue(modelMap.containsKey("farmId"));
 		assertSame((Integer) modelMap.get("farmId"), 1);
-		
+
 		assertTrue(modelMap.containsKey("success"));
 		assertSame(Views.SUCCESS.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testDeleteProduct() {
 		ModelMap modelMap = new ModelMap();
@@ -277,24 +269,24 @@ class ProvmanControllerTest {
 
 		assertTrue(modelMap.containsKey("returnPage"));
 		assertSame(Views.PRODUCT.getViewName(), (String) modelMap.get("returnPage"));
-		
+
 		assertTrue(modelMap.containsKey("farmId"));
 		assertSame((Integer) modelMap.get("farmId"), 1);
-		
+
 		assertTrue(modelMap.containsKey("success"));
 		assertSame(Views.SUCCESS.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testNewPricing() {
 		Product product = new Product();
 		ModelMap modelMap = new ModelMap();
 		Mockito.when(productService.findById(Mockito.any())).thenReturn(product);
-	
+
 		String retVal = controller.newPricing(1, modelMap);
 		assertTrue(modelMap.containsKey("product"));
 		assertSame((Product) modelMap.get("product"), product);
-		
+
 		assertTrue(modelMap.containsKey("pricing"));
 		assertSame(Views.PRICING.getViewName(), retVal);
 	}
@@ -303,81 +295,79 @@ class ProvmanControllerTest {
 	void testSavePricing() {
 		ModelMap modelMap = new ModelMap();
 		ProductPricing prodPricing = helperCreateProductPricing();
-	
+
 		String retVal = controller.savePricing(prodPricing, modelMap);
 		assertTrue(modelMap.containsKey("returnPage"));
 		assertSame(Views.PRICING.getViewName(), (String) modelMap.get("returnPage"));
-		
+
 		assertTrue(modelMap.containsKey("prodId"));
 		assertSame((Integer) modelMap.get("prodId"), 1);
-		
+
 		assertTrue(modelMap.containsKey("success"));
 		assertSame(Views.SUCCESS.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testGetPricings() {
 		ModelMap modelMap = new ModelMap();
 		List<ProductPricing> pricings = new ArrayList<ProductPricing>(List.of(new ProductPricing()));
-		Mockito.when(pricingService.findProductPricingsByProductId(Mockito.any()))
-				.thenReturn(pricings);
-		
+		Mockito.when(pricingService.findProductPricingsByProductId(Mockito.any())).thenReturn(pricings);
+
 		String retVal = controller.getPricings(1, modelMap);
 		assertTrue(modelMap.containsKey("pricings"));
-        @SuppressWarnings("unchecked")
+		@SuppressWarnings("unchecked")
 		List<ProductPricing> returnedList = (ArrayList<ProductPricing>) modelMap.get("pricings");
-        assertTrue(returnedList.size() == 1);
-        assertTrue(((ProductPricing) returnedList.get(0)).equals(pricings.get(0)));
+		assertTrue(returnedList.size() == 1);
+		assertTrue(((ProductPricing) returnedList.get(0)).equals(pricings.get(0)));
 
-        assertSame(Views.PRICINGLIST.getViewName(), retVal);    
+		assertSame(Views.PRICINGLIST.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testEditPricing() {
 		ModelMap modelMap = new ModelMap();
 		ProductPricing pricing = new ProductPricing();
-		Mockito.when(pricingService.findById(Mockito.any()))
-				.thenReturn(pricing);
-		
+		Mockito.when(pricingService.findById(Mockito.any())).thenReturn(pricing);
+
 		String retVal = controller.editPricing(1, modelMap);
 		assertTrue(modelMap.containsKey("pricing"));
-        assertSame((ProductPricing) modelMap.get("pricing"), pricing);
-        
-        assertTrue(modelMap.containsKey("edit"));
-        assertTrue((Boolean) modelMap.get("edit"));
-        
-        assertSame(Views.PRICING.getViewName(), retVal);    
+		assertSame((ProductPricing) modelMap.get("pricing"), pricing);
+
+		assertTrue(modelMap.containsKey("edit"));
+		assertTrue((Boolean) modelMap.get("edit"));
+
+		assertSame(Views.PRICING.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testUpdatePricing() {
 		ProductPricing pricing = helperCreateProductPricing();
 		ModelMap modelMap = new ModelMap();
-		
+
 		String retVal = controller.updatePricing(pricing, modelMap);
 		assertTrue(modelMap.containsKey("returnPage"));
 		assertSame(Views.PRICING.getViewName(), (String) modelMap.get("returnPage"));
-		
+
 		assertTrue(modelMap.containsKey("prodId"));
 		assertSame((Integer) modelMap.get("prodId"), 1);
-		
+
 		assertTrue(modelMap.containsKey("success"));
 		assertSame(Views.SUCCESS.getViewName(), retVal);
 	}
-	
+
 	@Test
 	void testDeletePricing() {
 		Integer prodId = 1;
-		ModelMap modelMap = new ModelMap();	
+		ModelMap modelMap = new ModelMap();
 
 		String retVal = controller.deletePricing(prodId, 1, modelMap);
 
 		assertTrue(modelMap.containsKey("returnPage"));
 		assertSame(Views.PRICING.getViewName(), (String) modelMap.get("returnPage"));
-		
+
 		assertTrue(modelMap.containsKey("prodId"));
 		assertSame((Integer) modelMap.get("prodId"), prodId);
-		
+
 		assertTrue(modelMap.containsKey("success"));
 		assertSame(Views.SUCCESS.getViewName(), retVal);
 	}
@@ -388,12 +378,12 @@ class ProvmanControllerTest {
 		pricing.getProduct().setId(1);
 		return pricing;
 	}
-	
+
 	private Product helperCreateProduct() {
 		Product product = new Product();
 		product.setId(1);
 		product.setFarmId(1);
 		return product;
 	}
-	
+
 }
